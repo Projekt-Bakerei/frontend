702a6925b480735d2eef9484d4925a4f81f18301
Vens Kolev <v.kolev@alfatrex.com>
2023-01-14 22:27:06 +0100

    Customer Artikel erstellen

--- src/Components/NewArtikel.js
+++ src/Components/NewArtikel.js
@@ -4,116 +4,79 @@ import { Box, Container } from "@mui/system";
 import Typography from "@mui/joy/Typography";
 import Autocomplete from "@mui/material/Autocomplete";
 import FormControl from "@mui/joy/FormControl";
-import { TextField } from "@mui/material";
-import { DataGrid } from '@mui/x-data-grid';
+import TextField from '@mui/material/TextField';
+//import { DataGrid } from '@mui/x-data-grid';
 
 import { useCustomer } from "../Context/CustomerContext";
 import { useUser } from "../Context/UserContext";
+import { useNewArtikel } from "../Context/ArtikelContext";
+
 import { FormLabel } from "@mui/joy";
-import { FormText } from "react-bootstrap";
+import { Form, FormText } from "react-bootstrap";
 
-const columns = [
-  { field: 'artikelKodu', headerName: 'Kodu', width: 100 },
-  // { field: 'kodu', headerName: 'Müsteri kodu', width: 100, sortable: true, },
-  {
-    field: 'artikelName',
-    headerName: 'Artikel',
-    width: 350,
-    editable: true,
-    sortable: true,
-  },
-  {
-    field: 'artikelBeschreibung',
-    headerName: 'Beschreibung',
-    type: 'text',
-    width: 550,
-    editable: true,
-    sortable: true,
-  },
-  //{
-  //  field: 'artikelPrice',
-   // headerName: 'Price',
-   // description: 'Einzel Price',
-   // sortable: true,
-   // width: 250,
-  //   valueGetter: (params) =>
-  //     `${params.row.cadde || ''}, ${params.row.plz || ''} ${params.row.yer|| ''}`,
- // },
-  {
-    field: 'artikelPrice',
-    headerName: 'Price',
-    type: 'number',
-    width: 120,
-    editable: true,
-    sortable: true,
-  },
-  // {
-  //   field: 'mobil',
-  //   headerName: 'Cep-Tel',
-  //   type: 'number',
-  //   width: 120,
-  //   editable: true,
-  //   sortable: true,
-  // },
-  // {
-  //   field: 'kategory',
-  //   headerName: 'Bar / Rechnung',
-  //   width: 120,
-  //   editable: true,
-  // },
-  // {
-  //   field: 'kdv',
-  //   headerName: 'KDV',
-  //   width: 120,
-  //   editable: true,
-  // },
-  // {
-  //   field: 'sekli',
-  //   headerName: 'Ödeme sekli',
-  //   width: 120,
-  //   editable: true,
-  // },
-  
-];
-
-const artikel = [
-  { id: "1", artikelName: 'Brot pide', artikelBeschreibung: '250 gr', artikelPrice: '0.80', artikelMenge: '25', },
-  { id: 2, artikelName: 'Lachmadgun', artikelBeschreibung: 'Halbar', artikelPrice: '1.80', artikelMenge: '120',  },
-  { id: 3, artikelName: 'Börek klein', artikelBeschreibung: 'Hackfleisch, Zwiebel, Petersilie', artikelPrice: '2.50', artikelMenge: '80', },
-  { id: 4, artikelName: 'Döner Pide', artikelBeschreibung: '180 gr', artikelPrice: '3.50', artikelMenge: '250', },
- 
-];
+import Table from 'react-bootstrap/Table';
 
 export const NewArtikel = () => {
   const { token } = useUser();
 
   const { listData } = useCustomer();
+  const { listNewArtikel } = useNewArtikel();
   console.log("List Costumers:", listData);
-
+  console.log("List Alle Artikel: ", listNewArtikel);
   const [listKunden, setListKunden] = useState([]);
+  const [listKundenArtikel, setListKundenArtikel] = useState([]);
+  const [kundeArtikel, setKundeArtikel] = useState([]);
+
 
   useEffect(() => {
     setListKunden(listData);
-  }, [listData]);
+    setListKundenArtikel(listNewArtikel);
+  }, [listData, listNewArtikel]);
+
 
   let kunden = listKunden;
   console.log("Kunden:", kunden);
 
+  let artikel = listKundenArtikel;
+
+
   const firmenMap = kunden.map(({ ismi }) => ismi);
   console.log("FirmenMap: ", firmenMap);
+  const artikelMap = artikel.map(({ NewartikelName }) => NewartikelName)
 
   const [value, setValue] = useState("Firma");
   const [inputValue, setInputValue] = useState("");
+  
+
+
+  const [valueArtikel, setValueArtikel] = useState("Artikel");
+  const [inputArtikelValue, setInputArtikelValue] = useState();
+  const [firmaData, setFirmaData] = useState([]);
 
   const Find = kunden.find((firma) => firma.ismi === `${value}`);
   console.log("Find: ", Find);
 
+  
+  //const rechnungFind = Find.forEach(element => console.log(element));
+//console.log("Rechnung find: ", rechnungFind);
+ //setFirmaData(rechnungFind)
+
+ 
+console.log("Firma data: ", firmaData)
+const FindArtikel = artikel.find((artikel) => artikel.NewartikelName === `${valueArtikel}`)
+
+// const handleChangeFirma = ()=> {
+//   setFirmaData(Find)
+// }
+
+
+console.log("State FirmaData: ", firmaData)
   return token ? (
     <Fragment>
       <CssBaseline />
       <Container maxWidth="xl">
         <h1>Ürün Dosyasi</h1>
-        <Box sx={{ bgcolor: "#cfe8fc", height: "75vh", padding: "1rem" }}>
+        <Box sx={{ bgcolor: "#cfe8fc", maxHeight: "80%", padding: "1rem" }}>
         <Box sx={{ flexGrow: 1 }}>
               <Box variant="soft" sx={{ py: 0.4 }}>
                 <Box className="d-flex p-3 justify-content-around">
@@ -167,20 +130,435 @@ export const NewArtikel = () => {
                 <hr />
               </Box>
             </Box>
-            {/* <Typography variant="soft" sx={{ py: 0.4 }}>
-              <code>
-                <strong>Find: {JSON.stringify(Find)}</strong>
-              </code>
-            </Typography> */}
             <br />
             <Box sx={{ height: '50%', width: '100%' }}>
-      <DataGrid
-        rows={artikel}
-        columns={columns}
-        pageSize={10}
-        rowsPerPageOptions={[10]}
-        experimentalFeatures={{ newEditingApi: true }}
-      />
+      <Table striped bordered hover variant="standart" responsive="sm">
+      <thead>
+        <tr>
+          <th style={{width: "5%"}}>Ürün kodu</th>
+          <th style={{width: "25%"}}>Ürün</th>
+          <th>Zutaten</th>
+          <th style={{width: "10%"}}>Price</th>
+          <th style={{width: "10%"}}>Rabat</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td style={{height: 3}}>  
+          <Autocomplete
+            placeholder="artikel"
+            value={valueArtikel}
+            onChange={(event, newValueArtikel) => {
+              setValueArtikel(newValueArtikel);
+            }}
+            inputArtikelValue={inputArtikelValue}
+            onInputChange={(event, newInputArtikelValue) => {
+              setInputArtikelValue(newInputArtikelValue);
+            }}
+            key={artikelMap}
+            options={artikelMap}
+            sx={{ width: 350, zIndex: 30 + "!important", borderRadius: 50 }}
+            renderInput={(params) => (
+              <TextField {...params} label="Artikel" />
+            )}
+          />
+        </td>
+          <td>
+            <FormLabel>
+             {FindArtikel !== undefined ? <b>{FindArtikel.NewartikelBeschreibung} </b> : " Zutaten"}
+          </FormLabel>
+          </td>
+          <td>
+          <TextField
+          id="standard-multiline-flexible"
+          label="Multiline"
+          multiline
+          maxRows={4}
+          variant="standard"
+        />
+          </td>
+          <td>
+          <TextField
+          id="standard-multiline-flexible"
+          label="Multiline"
+          multiline
+          maxRows={4}
+          variant="standard"
+        />
+          </td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>
+          <Autocomplete
+            placeholder="Müsteri ara"
+            value={value}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+            inputValue={inputValue}
+            onInputChange={(event, newInputValue) => {
+              setInputValue(newInputValue);
+            }}
+            key={firmenMap}
+            options={firmenMap}
+            sx={{ width: 350, zIndex: 30 + "!important", borderRadius: 50 }}
+            renderInput={(params) => (
+              <TextField {...params} label="Ismiyle | Müsteri ara" />
+            )}
+          />
+          </td>
+          <td>Thornton</td>
+          <td>
+          <TextField
+          id="standard-multiline-flexible"
+          label="Multiline"
+          multiline
+          maxRows={4}
+          variant="standard"
+        />
+          </td>
+          <td>
+          <TextField
+          id="standard-multiline-flexible"
+          label="Multiline"
+          multiline
+          maxRows={4}
+          variant="standard"
+        />
+          </td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>
+          <Autocomplete
+            placeholder="Müsteri ara"
+            value={value}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+            inputValue={inputValue}
+            onInputChange={(event, newInputValue) => {
+              setInputValue(newInputValue);
+            }}
+            key={firmenMap}
+            options={firmenMap}
+            sx={{ width: 350, zIndex: 30 + "!important", borderRadius: 50 }}
+            renderInput={(params) => (
+              <TextField {...params} label="Ismiyle | Müsteri ara" />
+            )}
+          />
+          </td>
+          <td>@twitter</td>
+          <td>
+          <TextField
+          id="standard-multiline-flexible"
+          label="Multiline"
+          multiline
+          maxRows={4}
+          variant="standard"
+        />
+          </td>
+          <td>
+          <TextField
+          id="standard-multiline-flexible"
+          label="Multiline"
+          multiline
+          maxRows={4}
+          variant="standard"
+        />
+          </td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>
+          <Autocomplete
+            placeholder="Müsteri ara"
+            value={value}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+            inputValue={inputValue}
+            onInputChange={(event, newInputValue) => {
+              setInputValue(newInputValue);
+            }}
+            key={firmenMap}
+            options={firmenMap}
+            sx={{ width: 350, zIndex: 30 + "!important", borderRadius: 50 }}
+            renderInput={(params) => (
+              <TextField {...params} label="Ismiyle | Müsteri ara" />
+            )}
+          />
+          </td>
+          <td>@twitter</td>
+          <td>
+          <TextField
+          id="standard-multiline-flexible"
+          label="Multiline"
+          multiline
+          maxRows={4}
+          variant="standard"
+        />
+          </td>
+          <td>
+          <TextField
+          id="standard-multiline-flexible"
+          label="Multiline"
+          multiline
+          maxRows={4}
+          variant="standard"
+        />
+          </td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>
+          <Autocomplete
+            placeholder="Müsteri ara"
+            value={value}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+            inputValue={inputValue}
+            onInputChange={(event, newInputValue) => {
+              setInputValue(newInputValue);
+            }}
+            key={firmenMap}
+            options={firmenMap}
+            sx={{ width: 350, zIndex: 30 + "!important", borderRadius: 50 }}
+            renderInput={(params) => (
+              <TextField {...params} label="Ismiyle | Müsteri ara" />
+            )}
+          />
+          </td>
+          <td>@twitter</td>
+          <td>
+          <TextField
+          id="standard-multiline-flexible"
+          label="Multiline"
+          multiline
+          maxRows={4}
+          variant="standard"
+        />
+          </td>
+          <td>
+          <TextField
+          id="standard-multiline-flexible"
+          label="Multiline"
+          multiline
+          maxRows={4}
+          variant="standard"
+        />
+          </td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>
+          <Autocomplete
+            placeholder="Müsteri ara"
+            value={value}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+            inputValue={inputValue}
+            onInputChange={(event, newInputValue) => {
+              setInputValue(newInputValue);
+            }}
+            key={firmenMap}
+            options={firmenMap}
+            sx={{ width: 350, zIndex: 30 + "!important", borderRadius: 50 }}
+            renderInput={(params) => (
+              <TextField {...params} label="Ismiyle | Müsteri ara" />
+            )}
+          />
+          </td>
+          <td>@twitter</td>
+          <td>
+          <TextField
+          id="standard-multiline-flexible"
+          label="Multiline"
+          multiline
+          maxRows={4}
+          variant="standard"
+        />
+          </td>
+          <td>
+          <TextField
+          id="standard-multiline-flexible"
+          label="Multiline"
+          multiline
+          maxRows={4}
+          variant="standard"
+        />
+          </td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>
+          <Autocomplete
+            placeholder="Müsteri ara"
+            value={value}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+            inputValue={inputValue}
+            onInputChange={(event, newInputValue) => {
+              setInputValue(newInputValue);
+            }}
+            key={firmenMap}
+            options={firmenMap}
+            sx={{ width: 350, zIndex: 30 + "!important", borderRadius: 50 }}
+            renderInput={(params) => (
+              <TextField {...params} label="Ismiyle | Müsteri ara" />
+            )}
+          />
+          </td>
+          <td>@twitter</td>
+          <td>
+          <TextField
+          id="standard-multiline-flexible"
+          label="Multiline"
+          multiline
+          maxRows={4}
+          variant="standard"
+        />
+          </td>
+          <td>
+          <TextField
+          id="standard-multiline-flexible"
+          label="Multiline"
+          multiline
+          maxRows={4}
+          variant="standard"
+        />
+          </td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>
+          <Autocomplete
+            placeholder="Müsteri ara"
+            value={value}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+            inputValue={inputValue}
+            onInputChange={(event, newInputValue) => {
+              setInputValue(newInputValue);
+            }}
+            key={firmenMap}
+            options={firmenMap}
+            sx={{ width: 350, zIndex: 30 + "!important", borderRadius: 50 }}
+            renderInput={(params) => (
+              <TextField {...params} label="Ismiyle | Müsteri ara" />
+            )}
+          />
+          </td>
+          <td>@twitter</td>
+          <td>
+          <TextField
+          id="standard-multiline-flexible"
+          label="Multiline"
+          multiline
+          maxRows={4}
+          variant="standard"
+        />
+          </td>
+          <td>
+          <TextField
+          id="standard-multiline-flexible"
+          label="Multiline"
+          multiline
+          maxRows={4}
+          variant="standard"
+        />
+          </td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td>
+          <Autocomplete
+            placeholder="Müsteri ara"
+            value={value}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+            inputValue={inputValue}
+            onInputChange={(event, newInputValue) => {
+              setInputValue(newInputValue);
+            }}
+            key={firmenMap}
+            options={firmenMap}
+            sx={{ width: 350, zIndex: 30 + "!important", borderRadius: 50 }}
+            renderInput={(params) => (
+              <TextField {...params} label="Ismiyle | Müsteri ara" />
+            )}
+          />
+          </td>
+          <td>@twitter</td>
+          <td>
+          <TextField
+          id="standard-multiline-flexible"
+          label="Multiline"
+          multiline
+          maxRows={4}
+          variant="standard"
+        />
+          </td>
+          <td>
+          <TextField
+          id="standard-multiline-flexible"
+          label="Multiline"
+          multiline
+          maxRows={4}
+          variant="standard"
+        />
+          </td>
+        </tr>
+        <tr>
+          <td>5</td>
+          <td>
+          <Autocomplete
+            placeholder="Müsteri ara"
+            value={value}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+            inputValue={inputValue}
+            onInputChange={(event, newInputValue) => {
+              setInputValue(newInputValue);
+            }}
+            key={firmenMap}
+            options={firmenMap}
+            sx={{ width: 350, zIndex: 30 + "!important", borderRadius: 50 }}
+            renderInput={(params) => (
+              <TextField {...params} label="Ismiyle | Müsteri ara" />
+            )}
+          />
+          </td>
+          <td>3</td>
+          <td>
+            <TextField
+          id="standard-multiline-flexible"
+          label="Multiline"
+          multiline
+          maxRows={4}
+          variant="standard"
+        />
+        </td>
+          <td>
+            <TextField
+          id="standard-multiline-flexible"
+          label="Multiline"
+          multiline
+          maxRows={4}
+          variant="standard"
+        />
+        </td>
+        </tr>
+      </tbody>
+    </Table>
     </Box>
         </Box>
 
