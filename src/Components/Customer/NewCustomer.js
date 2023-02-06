c7282e3a4dd52851d5e177e36617f8ad3576c019
Vens Kolev <v.kolev@alfatrex.com>
2023-02-06 11:15:04 +0100

    Orhan loading (#13)
    * loading component
    
    * isLoading
    
    * Loading working perfekt
    
    ---------
    
    Co-authored-by: Orhan Kadirov <orhan.kadirov.gs@gmail.com>

--- src/Components/Customer/NewCustomer.js
+++ src/Components/Customer/NewCustomer.js
@@ -12,9 +12,21 @@ import Form from "react-bootstrap/Form";
 
 import { useCustomer } from "../Context/CustomerContext";
 import { useUser } from "../Context/UserContext";
+import Loading from "../Loading/Loading";
 
 
 function NewCustomer() {
+
+  const [loading, setLoading] = useState(false);
+    const getDomainInfo = async () => {
+        try {
+            setLoading(true); // Set loading before sending API request
+            setLoading(false); // Stop loading
+        } catch (error) {
+            setLoading(false); // Stop loading in case of error
+            console.error(error);
+        }
+      }
   const bull = (
     <Box
       component="span"
@@ -550,6 +562,7 @@ function NewCustomer() {
                 variant="contained"
                 color="primary"
                 onClick={() => {
+                  loading ? <Loading/>:
                   addCustomer(
                     kodu,
                     passiv,
