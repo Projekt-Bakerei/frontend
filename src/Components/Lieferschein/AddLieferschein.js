a26347535149e3d1b71838d912051bf5aed62251
Vens Kolev <v.kolev@alfatrex.com>
2023-02-12 14:19:47 +0100

    Vens lieferschein (#15)
    * Aktuel Liferschein nummer mit LiefersceinNummer context erstellt
    
    * Aktuel Liferschein fix
    
    * Lieferschein state fix
    
    * Lieferschein context provider fix

--- src/Components/Lieferschein/AddLieferschein.js
+++ src/Components/Lieferschein/AddLieferschein.js
@@ -28,7 +28,9 @@ import { ArtikelTasksProvider } from "../Context/ArtikelTasksContext";
 //import Stack from '@mui/material/Stack';
 //import { DataGrid } from '@mui/x-data-grid';
 
-import { useLieferscheinNummer } from "../Context/LieferscheinContext";
+
+import {useLieferscheinContext} from "../Context/LieferscheinContext"
+
 
 // const columns = [
 
@@ -99,12 +101,15 @@ import { useLieferscheinNummer } from "../Context/LieferscheinContext";
 //   },
 // ];
 
-// Befor Jhre vechseln
+
+// Befor Jhre vechseln 
+
 let initialYear = 2023;
 
 // Actuel Datum Heute
 const date = new Date();
 let getYear = date.getFullYear();
+
 export const HeuteDatum = () => {
   const heute =
     String(date.getDate()).padStart(2, "0") +
@@ -116,19 +121,27 @@ export const HeuteDatum = () => {
 };
 
 export const AddLieferschein = () => {
+
   const { token } = useUser();
   const { listData } = useCustomer();
-  const { listLieferscheinNummer } = useLieferscheinNummer();
 
-  // LieferscheinNummer
-  const [lieferscheinNummer, setLieferscheinNummer] = useState({
-    lieferscheinNummerNew: "",
-  });
+  const { listLieferscheinNummer, loadLieferscheinNummer } = useLieferscheinContext();
+  // console.log(listLieferscheinNummer, loadLieferscheinNummer);
+  // // LieferscheinNummer
+  const [lieferscheinNummer, setLieferscheinNummer] = useState(
+    {
+      lieferscheinNummerNew: "",
+    }
+  );
 
-  const { lieferscheinNummerNew } = lieferscheinNummer;
+  // const {
+  //   lieferscheinNummerNew,
+  // } = lieferscheinNummer
 
   //Lieferschein State
-  const [newLieferscheinArtikeln, setNewLieferscheinArtikeln] = useState({
+  const [newLieferscheinArtikeln, setNewLieferscheinArtikeln] = useState(
+    
+    {
     artikelKoduLe: "",
     artikelNameLe: "",
     artikelMengeLe: "",
@@ -144,7 +157,8 @@ export const AddLieferschein = () => {
     artikelZutatenLe,
     artikelKistenzahlLe,
     artikelPriceLe,
-  } = newLieferscheinArtikeln;
+  }= newLieferscheinArtikeln;
+
 
   // Customer Daten einladen
   const [listKunden, setListKunden] = useState([]);
@@ -152,18 +166,18 @@ export const AddLieferschein = () => {
     setListKunden(listData);
   }, [listData, listLieferscheinNummer]);
 
-  // Lieferschein Aktuelnummer rechnen
+
+// Lieferschein Aktuelnummer rechnen
   let lieferscheinnummer = listLieferscheinNummer;
-  const newNummer = lieferscheinnummer.map(
-    (nummer) => nummer.LieferscheinNummer
-  );
+  const newNummer = lieferscheinnummer.map((nummer) => nummer.LieferscheinNummer);
   console.log("Nummer", newNummer);
   let newNummerLieferschein = Math.max.apply(null, newNummer);
-  if (getYear === initialYear) {
-    newNummerLieferschein += 1;
+  if (getYear === initialYear){
+  newNummerLieferschein += 1;
   } else {
     newNummerLieferschein += 1000000;
-  }
+}
+
   // Customer map
   let kunden = listKunden;
   const firmenMap = kunden.map(({ ismi }) => ismi);
@@ -242,14 +256,17 @@ export const AddLieferschein = () => {
     },
   ]);
 
-  const [inputArtikelLe, setInputArtikelLe] = useState([]);
+
+  const [inputArtikelLe, setInputArtikelLe] = useState([])
+
 
   const {
     inputArtikelNameIn,
     inputArtikelMengeIn,
     inputArtikelEinheitIn,
     inputArtikelKistenIn,
-  } = inputArtikel;
+
+  } = inputArtikel
 
   const handleInputChange = (e, index) => {
     const { name, value } = e.target;
@@ -259,7 +276,8 @@ export const AddLieferschein = () => {
   };
   const handleRemoveClick = (i) => {
     const list = [...inputArtikel];
-    list.splice(i, 1);
+     list.splice(i, 1);
+
     setInputArtikel(list);
   };
   const handleAddClick = () => {
@@ -278,22 +296,25 @@ export const AddLieferschein = () => {
   const onChangeArtikelLe = (e, index) => {
     // e.preventDefault();
     //const { name, value } = e.target;
+
     const list = [...inputArtikel];
     list[index].value = e.target.value;
     setInputArtikel(list);
     //setNewLieferscheinArtikeln({ ...newLieferscheinArtikeln, [e.target.name]: e.target.value });
   };
-
+  
   const handleChangeMenge = (e, index) => {
     e.preventDefault();
     const { name, value } = e.target;
     const list = [...inputArtikel];
     list[index][name] = value;
-    setInputArtikel([{ ...inputArtikel, [e.target.name]: e.target.value }]);
+    setInputArtikel([{...inputArtikel, [e.target.name]: e.target.value}] );
     // setInputArtikel({ ...inputArtikel, [e.target.name]: e.target.value });
-  };
 
+  };
+  
   console.log("New data: ", inputArtikel);
+
 
   // Print die Lieferschein
   const Print = () => {
@@ -303,11 +324,11 @@ export const AddLieferschein = () => {
     window.print();
     document.body.innerHTML = originalContents;
   };
-
+  
   // const ArtikelTabele = () => {
-
-  //   return (
-
+    
+    //   return (
+      
   //     <div>
   //     {/* <div>{`value: ${value !== null ? `'${value}'` : 'null'}`}</div>
   //     <div>{`inputValue: '${inputValue}'`}</div> */}
@@ -342,9 +363,9 @@ export const AddLieferschein = () => {
   //                     )}
   //                   />
   //   </div>
-
+        
   //       )
-
+    
   //     //   <Autocomplete
   //     //     id="highlights"
   //     //     sx={{ width: 300 }}
@@ -498,15 +519,19 @@ export const AddLieferschein = () => {
   //     //     )}
   //     //   </>
   //     // ))
-
+      
+    
   // };
 
-  // const [nbRows, setNbRows] = useState(0);
-  // const [product, setProduct] = useState("");
+
+    // const [nbRows, setNbRows] = useState(0);
+    // const [product, setProduct] = useState("");
+
 
   // const addRow = () => {
-
-  //   const length = FindArtikel.length;
+   
+  //   const length = FindArtikel.length; 
+    
 
   //   setNbRows(c => Math.min(length, c + 1)) ;
   //   if (findCustomer) {
@@ -550,7 +575,10 @@ export const AddLieferschein = () => {
                 <Box className="d-flex p-3 justify-content-between">
                   <Box className="d-flex flex-column">
                     <Typography level="body1">Kundenangaben</Typography>
-                    <FormText key={2}>
+
+                    <FormText
+                    key={2}>
+
                       {/* <Typography component="b">Firma: </Typography> */}
                       {findCustomer !== undefined ? (
                         <b>{findCustomer.hitab} </b>
@@ -563,7 +591,10 @@ export const AddLieferschein = () => {
                         " Name"
                       )}
                     </FormText>
-                    <FormText key={3}>
+
+                    <FormText
+                    key={3}>
+
                       {findCustomer !== undefined ? (
                         <b>{findCustomer.cadde}</b>
                       ) : (
@@ -577,7 +608,11 @@ export const AddLieferschein = () => {
                         " Straße & Nummer"
                       )}
                     </FormText>
-                    <FormText key={4}>
+
+                    <FormText
+                      key={4}
+                    >
+
                       {findCustomer !== undefined ? (
                         <b>{findCustomer.plz} </b>
                       ) : (
@@ -592,7 +627,10 @@ export const AddLieferschein = () => {
                   </Box>
                   <Box className="d-flex flex-column padding-right-6 w-25">
                     <Typography level="body1">
-                      Lieferschein -Nr.: {newNummerLieferschein}
+
+                      Lieferschein -Nr.:
+                      {newNummerLieferschein}
+
                     </Typography>
                     <FormText>
                       Kundennummer{" "}
@@ -641,84 +679,86 @@ export const AddLieferschein = () => {
                 </Typography>
               </div>
               {inputArtikel.map((x, i) => {
-                return (
-                  <>
-                    <div className="d-flex flex-wrap justify-content-around">
-                      <div className="d-flex border-top border-bottom">
-                        <TextField
-                          key={i}
-                          style={{ width: "2rem", height: "2rem" }}
-                          id="outlined-read-only-input"
-                          label={x.artikelKodu}
-                          name="artikelKodu"
-                          defaultValue={
-                            FindArtikel.artikelKodu !== undefined
-                              ? `${FindArtikel.artikelKodu}`
-                              : i + 1
-                          }
-                          InputProps={{
-                            readOnly: true,
-                          }}
-                          variant="standard"
-                          size="small"
-                        ></TextField>
-                        <Form.Label
-                          key={i}
-                          htmlFor="input"
-                          style={{
-                            //  marginRight: "2rem",
-                            //width: "18.5rem",
-                            fontFamily: "Roboto",
-                            fontSize: "0.875rem",
-                            fontWeight: 500,
-                          }}
-                        ></Form.Label>
-                        <Form.Select
-                          key={i}
-                          name="artikelNameLe"
-                          style={{ width: "20rem", height: "2rem" }}
-                          value={x.artikelName}
-                          onChange={onChangeArtikelLe}
-                          // onChange={e => {
-                          //   dispatch({
-                          //     type: 'changed',
-                          //     task: {
-                          //       ...task,
-                          //       NewartikelName: e.target.value
-                          //     }
-                          //   });
-                          // }}
-                        >
-                          {FindArtikel.map((artikel, k) => (
-                            <option name={artikel.artikelName} key={k}>
-                              {artikel.artikelName}
-                            </option>
-                          ))}
-                        </Form.Select>
 
-                        <TextField
+                  return (
+                    <>
+                      
+                      <div className="d-flex flex-wrap justify-content-around">
+                        <div className="d-flex border-top border-bottom">
+                          
+                          <TextField
                           key={i}
-                          style={{ width: "20rem", height: "2rem" }}
-                          id="outlined-read-only-input"
-                          //label="Read Only"
-                          defaultValue={FindArtikel[i]?.artikelBeschreibung}
-                          InputProps={{
-                            readOnly: true,
-                          }}
-                          variant="standard"
-                          size="small"
-                        />
-                        <Form.Control
+                            style={{ width: "2rem", height: "2rem" }}
+                            id="outlined-read-only-input"
+                            label={x.artikelKodu}
+                            name="artikelKodu"
+                            defaultValue={FindArtikel.artikelKodu !== undefined ? `${FindArtikel.artikelKodu}` : i}
+                            InputProps={{
+                              readOnly: true,
+                            }}
+                            variant="standard"
+                            size="small"
+                          ></TextField>
+                          <Form.Label
                           key={i}
-                          variant="outlined"
-                          style={{ width: "7rem", height: "2rem" }}
-                          name="inputArtikelMengeIn"
-                          placeholder="Menge"
-                          value={inputArtikelMengeIn}
-                          onChange={(e) => handleChangeMenge(e)}
-                        />
-                        <div
+                            htmlFor="input"
+                            style={{
+                              //  marginRight: "2rem",
+                              //width: "18.5rem",
+                              fontFamily: "Roboto",
+                              fontSize: "0.875rem",
+                              fontWeight: 500,
+                            }}
+                          >
+
+                          </Form.Label>
+                            <Form.Select
+                            key={i}
+                              name="artikelNameLe"
+                              style={{ width: "20rem", height: "2rem" }}
+                              value={x.artikelName}
+                              onChange={onChangeArtikelLe}
+                              // onChange={e => {
+                              //   dispatch({
+                              //     type: 'changed',
+                              //     task: {
+                              //       ...task,
+                              //       NewartikelName: e.target.value
+                              //     }
+                              //   });
+                              // }}
+                            >
+                              {FindArtikel.map((artikel, k) => (
+                                <option name={artikel.artikelName} key={k}>
+                                  {artikel.artikelName}
+                                </option>
+                              ))}
+                            </Form.Select>
+                          
+                          <TextField
                           key={i}
+                            style={{ width: "20rem", height: "2rem" }}
+                            id="outlined-read-only-input"
+                            //label="Read Only"
+                            defaultValue={FindArtikel[0]?.artikelBeschreibung}
+                            InputProps={{
+                              readOnly: true,
+                            }}
+                            variant="standard"
+                            size="small"
+                          />
+                          <Form.Control
+                          key={i}
+                            variant="outlined"
+                            style={{ width: "7rem", height: "2rem" }}
+                            name="inputArtikelMengeIn"
+                            placeholder="Menge"
+                            value={inputArtikelMengeIn}
+                            onChange={(e) => handleChangeMenge(e)}
+                          />
+                          <div 
+                            key={i}
+
                           className="border border-dark"
                           style={{ width: "7rem", height: "2rem" }}
                         ></div>
@@ -732,41 +772,42 @@ export const AddLieferschein = () => {
                           /> */}
                         <Form.Control
                           key={i}
-                          style={{ width: "7rem", height: "2rem" }}
-                          name="kisten"
-                          placeholder="Kisten"
-                          //value={artikelKistenLe}
-                          //onChange={(e) => handleKistenChange()}
-                        />
+                            style={{ width: "7rem", height: "2rem" }}
+                            name="kisten"
+                            placeholder="Kisten"
+                            //value={artikelKistenLe}
+                            //onChange={(e) => handleKistenChange()}
+                          />
+                        </div>
+                        <div
+                          key={i}
+                          className="d-flex flex-row gap-1">
+                          {inputArtikel.length  !== 1 && (
+                            <Button
+                              color="error"
+                              variant="outlined"
+                              style={{ width: "5rem", height: "2rem" }}
+                              onClick={() => handleRemoveClick(i)}
+                            >
+                              Löschen
+                            </Button>
+                          )}
+                        </div>
                       </div>
-                      <div key={i} className="d-flex flex-row gap-1">
-                        {inputArtikel.length !== 1 && (
-                          <Button
-                            color="error"
-                            variant="outlined"
-                            style={{ width: "5rem", height: "2rem" }}
-                            onClick={() => handleRemoveClick(i)}
-                          >
-                            Löschen
-                          </Button>
-                        )}
-                      </div>
-                    </div>
-                    {!findCustomer
-                      ? null
-                      : inputArtikel.length - 1 === i && (
-                          <Button
-                            color="success"
-                            variant="outlined"
-                            style={{ width: "5rem", height: "2rem" }}
-                            onClick={handleAddClick}
-                          >
-                            Neue
-                          </Button>
-                        )}
-                  </>
-                );
-              })}
+                      {!findCustomer ? (null): (inputArtikel.length - 1 === i && (
+                        <Button
+                          color="success"
+                          variant="outlined"
+                          style={{ width: "5rem", height: "2rem" }}
+                          onClick={handleAddClick}
+                        >
+                          Neue
+                        </Button>
+                      ))}
+                    </>
+                  );
+                })}
+
             </Box>
           </Box>
           <div className="d-flex flex-wrap justify-content-start">
@@ -797,17 +838,19 @@ export const AddLieferschein = () => {
           </div>
           <div className="d-flex flex-wrap flex-row-reverse">
             <div className="d-flex flex-wrap mt-3 gap-3 pb-5">
+
+                <Button
+                  variant="outlined"
+                  sx={{
+                    height: 40,
+                  }}
+              >
+                <BiSave/>
+                  &nbsp;Zwischenspeichern
+                </Button>
+
               <Button
                 variant="outlined"
-                sx={{
-                  height: 40,
-                }}
-              >
-                <BiSave />
-                &nbsp;Zwischenspeichern
-              </Button>
-              <Button
-                variant="contained"
                 sx={{
                   height: 40,
                 }}
